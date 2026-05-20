17d6bac9f8ad5f9c46ba2745353db6877b22e2f0
techroomak <techroomak.info@gmail.com>
2026-05-20 13:29:18 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,149 @@
+import { db }
+from "./firebase.js";
 
+import {
+doc,
+setDoc,
+getDoc,
+updateDoc,
+increment
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+/* ========================= */
+/* TELEGRAM */
+/* ========================= */
+
+const tg =
+window.Telegram.WebApp;
+
+tg.ready();
+
+tg.expand();
+
+const user =
+tg.initDataUnsafe?.user;
+
+
+/* ========================= */
+/* USER INFO */
+/* ========================= */
+
+const userId =
+user?.id || "123456789";
+
+const username =
+user?.first_name ||
+user?.username ||
+"Money Maker BD";
+
+const photo =
+user?.photo_url ||
+"https://telegram.org/img/t_logo.png";
+
+
+/* ========================= */
+/* UI UPDATE */
+/* ========================= */
+
+document.getElementById("username")
+.innerText = username;
+
+document.getElementById("userid")
+.innerText = userId;
+
+document.getElementById("profile")
+.src = photo;
+
+
+/* ========================= */
+/* DATABASE */
+/* ========================= */
+
+const userRef =
+doc(db,"users",String(userId));
+
+const snap =
+await getDoc(userRef);
+
+if(!snap.exists()){
+
+await setDoc(userRef,{
+
+username: username,
+coin: 0,
+refer: 0,
+referEarn: 0,
+withdraw: 0,
+dailyEarn: 0,
+created: Date.now()
+
+});
+
+}
+
+const userData =
+(await getDoc(userRef)).data();
+
+
+/* ========================= */
+/* UI DATA */
+/* ========================= */
+
+document.getElementById("coin")
+.innerText =
+userData.coin;
+
+document.getElementById("bdt")
+.innerText =
+(userData.coin/10).toFixed(2);
+
+document.getElementById("totalRefer")
+.innerText =
+userData.refer;
+
+document.getElementById("dailyEarn")
+.innerText =
+userData.dailyEarn;
+
+document.getElementById("referEarn")
+.innerText =
+userData.referEarn;
+
+document.getElementById("totalWithdraw")
+.innerText =
+userData.withdraw;
+
+
+/* ========================= */
+/* CLAIM COIN */
+/* ========================= */
+
+window.claimCoin =
+async(amount)=>{
+
+await updateDoc(userRef,{
+coin: increment(amount),
+dailyEarn: increment(amount)
+});
+
+location.reload();
+
+};
+
+
+/* ========================= */
+/* COPY INVITE */
+/* ========================= */
+
+window.copyInvite = ()=>{
+
+const link =
+`https://t.me/emoneymakebd_bot/app?startapp=${userId}`;
+
+navigator.clipboard.writeText(link);
+
+alert("Invite Link Copied");
+
+};

5d5dacffa7014a01ad5d810b0f47868d996b08d0
techroomak <techroomak.info@gmail.com>
2026-05-21 17:11:23 +0600

    Update auth.js

--- admin/js/auth.js
+++ admin/js/auth.js
@@ -1,63 +1,16 @@
-// =========================
-// FIREBASE
-// =========================
-
-import { db }
-from "./firebase.js";
-
-import {
-doc,
-getDoc
-}
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-/* ========================= */
-/* SECRET ADMIN ACCESS */
-/* ========================= */
-
 const url =
 new URL(window.location.href);
 
 const key =
 url.searchParams.get("key");
 
-// SECRET KEY
-
 if(key !== "AKADMIN2026"){
 
 document.body.innerHTML =
+"Access Denied";
 
-`
-
-<div style="
-
-height:100vh;
-display:flex;
-justify-content:center;
-align-items:center;
-background:#020617;
-color:white;
-font-size:28px;
-font-family:sans-serif;
-
-">
-
-Access Denied
-
-</div>
-
-`;
-
-throw new Error(
-"Unauthorized Access"
-);
+throw new Error("Unauthorized");
 
 }
 
-/* ========================= */
-/* ADMIN ACCESS */
-/* ========================= */
-
-console.log(
-"Admin Access Granted"
-);
+console.log("Admin Access Granted");

8f6688349306fbde19ac37640d170c3760c1ca24
techroomak <techroomak.info@gmail.com>
2026-05-21 17:05:24 +0600

    Update auth.js

--- admin/js/auth.js
+++ admin/js/auth.js
@@ -12,35 +12,51 @@ getDoc
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 /* ========================= */
-/* TELEGRAM */
+/* SECRET ADMIN ACCESS */
 /* ========================= */
 
-const tg =
-window.Telegram.WebApp;
+const url =
+new URL(window.location.href);
 
-tg.ready();
+const key =
+url.searchParams.get("key");
 
-const user =
-tg.initDataUnsafe.user;
+// SECRET KEY
 
-// BLOCK OUTSIDE TG
+if(key !== "AKADMIN2026"){
 
+document.body.innerHTML =
 
+`
+
+<div style="
+
+height:100vh;
+display:flex;
+justify-content:center;
+align-items:center;
+background:#020617;
+color:white;
+font-size:28px;
+font-family:sans-serif;
+
+">
+
+Access Denied
+
+</div>
+
+`;
+
+throw new Error(
+"Unauthorized Access"
+);
+
+}
 
 /* ========================= */
-/* ADMIN CHECK */
+/* ADMIN ACCESS */
 /* ========================= */
-
-const adminRef =
-doc(db,"admin",String(user.id));
-
-const adminSnap =
-await getDoc(adminRef);
-
-// NOT ADMIN
-
-
-// ADMIN OK
 
 console.log(
 "Admin Access Granted"

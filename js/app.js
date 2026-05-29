ac0dd1fce4b60711537f5cd6af8c3d61bb73b1ce
techroomak <techroomak.info@gmail.com>
2026-05-29 18:28:33 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -393,12 +393,6 @@ if(ad2) ad2.innerText = `${updatedData.ad2Count || 0}/20`;
 if(ad3) ad3.innerText = `${updatedData.ad3Count || 0}/15`;
 if(ad4) ad4.innerText = `${updatedData.ad4Count || 0}/10`;
 }
-async function loadUserData(){
-
-  // code
-
-}
-
 await loadUserData();
 /* ========================= */
 /* DAILY TIMER */

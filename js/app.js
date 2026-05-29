10e7c07c584d98e9fe5f1973a6158aab41578f0c
techroomak <techroomak.info@gmail.com>
2026-05-29 18:27:57 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -388,12 +388,18 @@ const ad2 = document.getElementById("ad2Limit");
 const ad3 = document.getElementById("ad3Limit");
 const ad4 = document.getElementById("ad4Limit");
 
-}
 if(ad1) ad1.innerText = `${updatedData.ad1Count || 0}/25`;
 if(ad2) ad2.innerText = `${updatedData.ad2Count || 0}/20`;
 if(ad3) ad3.innerText = `${updatedData.ad3Count || 0}/15`;
 if(ad4) ad4.innerText = `${updatedData.ad4Count || 0}/10`;
+}
+async function loadUserData(){
 
+  // code
+
+}
+
+await loadUserData();
 /* ========================= */
 /* DAILY TIMER */
 /* ========================= */

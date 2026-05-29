d81498be9545861c5579938ec9ba9226fb258a1d
techroomak <techroomak.info@gmail.com>
2026-05-29 18:32:23 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -392,8 +392,11 @@ if(ad1) ad1.innerText = `${updatedData.ad1Count || 0}/25`;
 if(ad2) ad2.innerText = `${updatedData.ad2Count || 0}/20`;
 if(ad3) ad3.innerText = `${updatedData.ad3Count || 0}/15`;
 if(ad4) ad4.innerText = `${updatedData.ad4Count || 0}/10`;
+
+if(document.getElementById("coin")){
+   await loadUserData();
 }
-await loadUserData();
+}
 /* ========================= */
 /* DAILY TIMER */
 /* ========================= */

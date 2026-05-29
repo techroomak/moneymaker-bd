e1ecb8ef98ede0cd200ff18ff4f4728b951260a3
techroomak <techroomak.info@gmail.com>
2026-05-29 13:07:24 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -541,24 +541,7 @@ return;
 
 }
 
-// DAILY LIMIT Reset
-const now = Date.now();
 
-if(
-userData[adLastField] &&
-(now - userData[adLastField]) >
-(24 * 60 * 60 * 1000)
-){
-
-await updateDoc(userRef,{
-
-[adCountField]:0
-
-});
-
-userData[adCountField] = 0;
-
-}
 // DAILY LIMIT
 
 if(dailyWithdrawCount >= 3){

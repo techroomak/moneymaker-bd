a3581bc2a22640eb429e17163454982ee2b7b482
techroomak <techroomak.info@gmail.com>
2026-06-02 10:49:43 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -246,13 +246,25 @@ refSnap.data();
 if(refData.banned !== true){
 
   await updateDoc(refUserRef,{
-    coin:increment(15),
+    coin:increment(10),
     refer:increment(1),
-    referEarn:increment(15)
+    referEarn:increment(10)
   });
 
 }
 
+await updateDoc(refUserRef,{
+  refer:increment(1)
+});
+
+if(refData.banned !== true){
+
+  await updateDoc(refUserRef,{
+    coin:increment(10),
+    referEarn:increment(10)
+  });
+
+}
 // SAVE REFERRER INFO
 
 await updateDoc(userRef,{
